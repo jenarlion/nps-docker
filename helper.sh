c59d894ace776b400b4d95c600f14557153e6a76
lihaixin <lihaixin@15099.net>
2022-05-01 17:48:45 +0800

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -4,11 +4,13 @@ export PATH
 
 cat > /conf/npc.conf<< TEMPEOF
 [common]
-server_addr=127.0.0.1:$BRIDGE_PORT
+server_addr=$DOMAIN:$BRIDGE_PORT
 conn_type=$MODE
 vkey=$PUBLIC_VKEY
 auto_reconnection=true
 remark=$DOCKERID
 TEMPEOF
 
+# https://github.com/sjourdan/alpine-sshd
+dropbear -RFm -p 22
 npc
