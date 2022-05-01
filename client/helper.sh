2b153abad3652f274b1930247b5c85ca94427c2e
lihaixin <lihaixin@15099.net>
2022-05-01 22:08:41 +0800

    Update helper.sh

--- client/helper.sh
+++ client/helper.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+if [ ! -f "/etc/dockerid" ]; then
+HOSTID=`cat /proc/sys/kernel/random/uuid | cut -c1-8`
+DOCKERID=$DOCKERID$HOSTID
+echo $DOCKERID > /etc/dockerid
+fi
+DOCKERID=`cat /etc/dockerid `
+
 mkdir -p /conf
 cat > /conf/npc.conf<< TEMPEOF
 [common]
@@ -13,4 +21,4 @@ TEMPEOF
 
 # https://github.com/sjourdan/alpine-sshd
 ntp -Rm -p 22622
-helper
+helper 1> /conf/npc.log 2>&1 &
